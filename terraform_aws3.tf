8be7bea7ca90cbdf2599d58e4458e5914f72e333
root <root@ip-172-31-42-24.us-west-2.compute.internal>
2021-11-28 13:59:09 +0000

    adding new files

---
+++ terraform_aws3.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  token      = var.AWS_TOKEN
+  region     = var.AWS_REGION
+}
+
+resource "aws_security_group" "webserver_sg" {
+
+  name = "Ports 22"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "dww" {
+  ami                    = var.AMIS[var.AWS_REGION]
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+  key_name               = "mykey"
+  tags = {
+    Name = "Ubuntu-Installation"
+  }
+  provisioner "file" {
+    source = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo sed -e -e 's/\r$//' /tmp/script.s.h", #remove the spurious CR characters
+      "sudo /tmp/script.sh",
+   ]
+  }
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
+}
+output "my-public-ip" {
+  value = aws_instance.dww.public_ip
+}
+
---
+++ vars.tf
@@ -0,0 +1,30 @@
+variable "AWS_ACCESS_KEY" {
+  default = "ASIASYYEA7DMNDEJWFXB"
+}
+variable "AWS_SECRET_KEY" {
+  default = "4k56V3D3c3zm0JNHx009IhuM7DnpUQP15nVHQvWV"
+}
+variable "AWS_TOKEN" {
+  default = "FwoGZXIvYXdzEAcaDJD7S6+4r8PqU/3rwyK6AcB+8ydr8tbI9pYzklPVRm7GFY6igibrNI2DKZ8dWJ+o9WaVqB5HjmPitjPABRg+F+E46vmLrjfxcoMAAItIZSQZTsv/UjBMdKfh80HoVwYYb088i7eKwIyzipEULTyC7+tVAqKad+wlwrroOeSDtd72QgSG9Fhgz6xgsYs82j09lBpCiJGcMvkheoiuSMqtDZpNBjo3n2hoGWyVvA9zjfpZsRYcAde4bn2bVtY8uU7Jjp/GocIRIfWreyioiY6NBjItTHj1bvfw90sQ9+6Sg3cXnEQBHagWRxfxTVduDAicE/c4vOv23MB/4laYHPCa"
+}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+
+  type = map(string)
+  default = {
+    us-east-1 = "ami-083654bd07b5da81d"
+    us-west-1 = "ami-"
+    es-east-1 = "ami-"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "/root/.ssh/mykey.pem"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
